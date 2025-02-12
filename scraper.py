0e7b22b350d5bd3d832aea2eef38a1953c596fd8
lescobedo <lescobedoai@gmail.com>
2025-02-11 19:47:02 -0500

    reduced comments

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-load_dotenv()  # Load environment variables from .env file
+load_dotenv()  # Load env vars from .env file
 import psycopg2
 import logging
 import requests
